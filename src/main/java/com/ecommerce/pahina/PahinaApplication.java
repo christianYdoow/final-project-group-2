35638cd9bf3d632630c491c9c95026293af83347
Sammuel Tuliao <sammuel.tuliao@novare.com.hk>
2023-04-17 12:53:53 +0800

    Initial commit

---
+++ src/main/java/com/ecommerce/pahina/PahinaApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.pahina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PahinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PahinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/pahina/PahinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.pahina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PahinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
